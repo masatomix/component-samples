d2fb82187f5c15d1eb402de6d44a28e3ed90435b
Masatomi KINO <masatomix@ki-no.org>
2019-11-04 03:06:32 +0900

    Componentテスト

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,98 +1,77 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+    <!-- https://jp.vuejs.org/v2/guide/components-custom-events.html#sync-%E4%BF%AE%E9%A3%BE%E5%AD%90 -->
+    <h2>子で値を変更パタン(.sync版)(Test.vue)</h2>
+    <Test :param.sync="p1" :paramObj.sync="p2"></Test>
+    [{{ p1 }}] [{{ p2 }}]
+
+    <!-- .syncの書き方は、下記バージョンのsyntactic sugarといえる -->
+    <h2>子で値を変更パタン(.sync未使用パタン)(Test.vue)</h2>
+    <Test
+      :param="p1"
+      :paramObj="p2"
+      @update:param="p1 = $event"
+      @update:paramObj="p2 = $event"
+    ></Test>
+    [{{ p1 }}] [{{ p2 }}]
+    <hr />
+
+    <h2>親にemitで通知して、親で値を変更させるパタン(Test2.vue)</h2>
+    <Test2 :param="p3" :paramObj="p4" @attack="aMethod"></Test2>
+    [{{ p3 }}] [{{ p4 }}]
+    <hr />
+
+    <h2>v-model(syntactic sugarナシバージョン)(Test3.vue)</h2>
+    <Test3 :value="p5" @input="p5 = $event"></Test3>
+    [{{ p5 }}]
+
+    <h2>v-model(syntactic sugarバージョン)(Test3.vue)</h2>
+    <Test3 v-model="p5"></Test3>
+    [{{ p5 }}]
+    <hr />
+
+    <h2>v-model(Objectの場合)(Test4.vue)</h2>
+    <Test4 :value="p6" @input="p6 = $event"></Test4>
+    [{{ p6.param1 }}]
+
+    <h2>v-model(Objectの場合)(Test4.vue)</h2>
+    <Test4 v-model="p6"></Test4>
+    [{{ p6.param1 }}]
+    <hr />
   </div>
 </template>
 
 <script>
+// https://jp.vuejs.org/v2/guide/components.html
+import Test from './Test.vue'
+import Test2 from './Test2.vue'
+import Test3 from './Test3.vue'
+import Test4 from './Test4.vue'
+
 export default {
-  name: "HelloWorld",
-  props: {
-    msg: String
-  }
-};
+  name: 'HelloWorld',
+  components: {
+    Test,
+    Test2,
+    Test3,
+    Test4,
+  },
+  data: () => ({
+    p1: 1,
+    p2: { param1: 2 },
+
+    p3: 3,
+    p4: { param1: 4 },
+    p5: 5,
+    p6: { param1: 6 },
+  }),
+  methods: {
+    aMethod() {
+      this.p3 += 3
+      this.p4.param1 -= 3
+    },
+  },
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
